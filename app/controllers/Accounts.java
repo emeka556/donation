b72f38a39673d215012a65e777442dccb8aa43e2
Emeka Okonji <emekaokonji@live.com>
2016-06-03 12:51:44 +0100

    Baseline work done on Account and User Java

---
+++ app/controllers/Accounts.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import models.User;
+import play.mvc.Controller;
+
+public class Accounts extends Controller{
+
+	  public static void signup()
+	  {
+	    render();
+	  }
+	  
+	  public static void register(User user)
+	  {
+	    user.save();
+	    login();
+	    
+	  }
+
+	  public static void login()
+	  {
+	    render();
+	  }
+
+	  public static void logout()
+	  {
+	    session.clear();
+	    Welcome.index();
+	  }
+
+}
