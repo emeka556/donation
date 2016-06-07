d2257dd84061ffcb12b86f6fccb030f30501c8f4
Emeka Okonji <emekaokonji@live.com>
2016-06-07 10:40:31 +0100

    Updated version of v0 signup - stored in database

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -1,44 +1,66 @@
 package controllers;
 
-import models.User;
-import play.Logger;
-import play.mvc.Controller;
+import play.*;
+import play.mvc.*;
+import java.util.*;
+import models.*;
 
-public class Accounts extends Controller{
+//@javax.persistence.Entity 
 
-	  public static void signup()
-	  {
-	    render();
-	  }
-	  
-	  public static void register(User user)
-	  {
-	    user.save();
-	    login();	    
-	  }
+public class Accounts extends Controller
+{
 
-	  public static void login()
-	  {
-	    render();
-	  }
+  public static void signup()
+  {
+    render();
+  }
 
-	  public static void logout()
-	  {
-	    session.clear();
-	    Welcome.index();
-	  }
-	  
-	  public static User getCurrentUser()
-	  {
-	    String userId = session.get("logged_in_userid");
-	    if (userId == null)
-	    {
-	      return null;
-	    }
-	    User logged_in_user = User.findById(Long.parseLong(userId));
-	    Logger.info("In Accounts controller: Logged in user is " + logged_in_user.firstName);
-	    return logged_in_user;
-	  }
+  public static void register(User user)
+  {
+    user.save();
 
+    login();
+  }
 
+  public static void login()
+  {
+    render();
+  }
+
+  public static void logout()
+  {
+
+    session.clear();
+    Welcome.index();
+  }
+
+  /*public static void authenticate(String email, String password)
+  {
+    Logger.info("Attempting to authenticate with " + email + ":" + password);
+
+    User user = User.findByEmail(email);
+    if ((user != null) && (user.checkPassword(password) == true))
+    {
+      Logger.info("Successful authentication of  " + user.firstName + " " + user.lastName);
+      session.put("logged_in_userid", user.id);
+      DonationController.index();
+    }
+    else
+    {
+      Logger.info("Authentication failed");
+      login();
+    }
+  }*/
+
+  public static User getCurrentUser()
+  {
+    String userId = session.get("logged_in_userid");
+    if (userId == null)
+    {
+      return null;
+    }
+    User logged_in_user = User.findById(Long.parseLong(userId));
+    Logger.info("In Accounts controller: Logged in user is " + logged_in_user.firstName);
+    return logged_in_user;
+  }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -2,39 +2,53 @@ package models;
 
 import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
+//import javax.persistence.*;
+import javax.persistence.Entity;
+//import javax.persistence.EntityManager;
+//import javax.persistence.Table;
 
 import play.db.jpa.Model;
 
-public class User extends Model 
-{
- public boolean usaCitizen;
- public String firstName;
- public String lastName;
- 
-// @OneToMany(mappedBy = "from" , cascade = CascadeType.ALL)
- //List<Donation> donations = new ArrayList<Donation>();
- public String email;
- public String password;
- 
- public User(boolean usaCitizen, String firstName, String lastName, String email, String password){
-	 this.usaCitizen = usaCitizen;
-	 this.firstName = firstName;
-	 this.lastName = lastName;
-	 this.email = email;
-	 this.password = password;
+
+
+//@Table(name = "`User`")
+//@javax.persistence.Entity
+
+//@PersistenceContext
+
+
+
+@Entity
+
+public class User extends Model {
+	//EntityManager em;
+	public boolean usCitizen;
+	public String firstName;
+	public String lastName;
 	 
- }
- 
- public static User findByEmail( String email){
-     return find("email", email).first();
-	 
-	 
- }
- public boolean checkPassword( String password){
-     return this.password.equals(password);
-	 
-	 
- }
- 
- 
+
+	// @OneToMany(mappedBy = "from" , cascade = CascadeType.ALL)
+	// List<Donation> donations = new ArrayList<Donation>();
+	public String email;
+	public String password;
+
+	public User(boolean usCitizen, String firstName, String lastName, String email, String password) {
+		this.usCitizen = usCitizen;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+
+	}
+
+	public static User findByEmail(String email) {
+		return find("email", email).first();
+
+	}
+
+	public boolean checkPassword(String password) {
+		return this.password.equals(password);
+
+	}
+
 }
